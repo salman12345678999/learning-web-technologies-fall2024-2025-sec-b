35258d0e6d8dff52283f387f1a509cf596cb83ab
salman12345678999 <159416107+salman12345678999@users.noreply.github.com>
2024-12-30 23:28:39 +0600

    Delete webB directory

--- webB/contoller/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    //unset($_SESSION['xyz']);
-    //session_destroy();
-    setcookie('status', 'true', time()-10, '/');
-    header('location: ../view/login.html');
-
-?>
